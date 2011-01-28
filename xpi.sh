dff84e79e94665aed3b8f75e1d9618f2c1025cb1
Norman Krämer <kraemer.norman@googlemail.com>
2011-01-28 16:59:17 +0100

    xpi file has the version number in the filename, which is taken from install.rdf

--- xpi.sh
+++ xpi.sh
@@ -1,1 +1,9 @@
-zip -r otrswatcher.xpi install.rdf chrome/ chrome.manifest defaults/
+#! /bin/bash
+
+makexpi(){
+ version=$1
+  zip -r "otrswatcher-$1.xpi" install.rdf chrome/ chrome.manifest defaults/
+}
+
+version=$(<install.rdf sed -n "/<em:version>/! b; s|<em:version>\(.*\)</em:version>|\1| p")
+makexpi $version
